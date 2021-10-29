9e5a2018256008ced94b9d0502b386607a518a58
amsesnic <asesnic@pmfst.hr>
2021-10-29 19:49:33 +0200

    Update

--- Vjezbe_4/Analyzer.h
+++ Vjezbe_4/Analyzer.h
@@ -6,13 +6,12 @@
 
 using namespace std; 
 
-
 class Analyzer{
 	public:
 		string particleName;
 		double mass;
 		bool ifBoson;
-		double momentum[4];
+		double momentum[4], transP;
 		bool _skipFirstLine;
 
 		void ReadFile(string input_file);
--- Vjezbe_4/analyze.cpp
+++ Vjezbe_4/analyze.cpp
@@ -1,9 +1,5 @@
 #include "Analyzer.h"
 
-#define N_HIGGS 10000
-#define P_MIN -100.
-#define P_MAX  100.
-
 #define IME_DAT "Analysis.txt"
 
 using namespace std;
@@ -12,7 +8,7 @@ int main(){
 	Analyzer* a;
 	a = new Analyzer();
 
-	a->ReadFile("Analysis.txt");	
+	a->ReadFile(IME_DAT);	
 
 	delete a;
 	return 0;
