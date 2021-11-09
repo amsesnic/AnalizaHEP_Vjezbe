564b7fb305ab4c731abe36f8ec2f7921655a6059
amsesnic <asesnic@pmfst.hr>
2021-11-09 22:07:35 +0100

    Zad5 sredivanje grafa

--- Vjezbe_5/Analyzer.cpp
+++ Vjezbe_5/Analyzer.cpp
@@ -12,48 +12,47 @@
 void Analyzer::PlotHistogram()
 {
     TCanvas c("c", "Transverse momentum", 0,0,400,300);
-    TH1F *h1 = new TH1F("h1", "Transverse P 1",     70, 0., 140.);
-    TH1F *h2 = new TH1F("h2", "Transverse P 2",     70, 0., 140.);
-    TH1F *hH = new TH1F("hH", "Transverse P Higgs", 70, 0., 140.);
-    TFile *fout = TFile::Open("H_TransP_12+H_fin.root", "RECREATE");
+    TH1F *h1     = new TH1F("h1", "Transverse P 1",     70, 0., 140.);
+    TH1F *h2     = new TH1F("h2", "Transverse P 2",     70, 0., 140.);
+    TH1F *hH     = new TH1F("hH", "Transverse P Higgs", 70, 0., 140.);
+    TFile *fout  = TFile::Open("H_TransP_12+H_fin.root", "RECREATE");
+    TLegend *leg = new TLegend(0.6, 0.7, 0.9, 0.9); // constructor takes coord of lower left and upper right corners
 
-    c.Divide(2, 1, 0.01, 0.01);  //divide canvas into 2 sub-pads
+
+    c.Divide(2, 1, 0, 0);  //divide canvas into 2 sub-pads
     gStyle->SetOptStat(0);       //remove statistics boxes
     
     this->Loop(h1, h2, hH); // Punjenje je u metodi Loop(TH1F* h...)
     
-    c.cd(1); //set sub-pad 1 as current pad (pointer gPad) DRAW HERE!
+    c.cd(1); //set sub-pad 1 as current pad (pointer gPad) DRAW() HERE!
     h1->GetXaxis()->SetTitle("P_t (GeV/c)");
     h1->GetYaxis()->SetTitle("Number of particles");
     h1->SetLineColor(kMagenta);
     h1->SetFillColor(kMagenta);
-    //h1->SetStats(0);
     h1->SetTitle("Transverse momentum of decay particles");
     h1->Draw();
 
-    //h2->GetXaxis()->SetTitle("P_t (GeV/c)");
-    //h2->GetYaxis()->SetTitle("Number of particles");
     h2->SetLineColor(kGreen+2);
-    //h2->SetFillColor(kMagenta);
-    //h2->SetStats(0);
     h2->SetTitle("");
     h2->Draw("same"); // same canvas, aka sub-pad!
+
+    //leg->SetHeader("Higgs boson decay simulation", "C");
+    leg->AddEntry(h1, "Decay Particle 1", "f");
+    leg->AddEntry(h2, "Decay Particle 2", "l");
+    //leg->AddEntry(hH, "Higgs boson", "l");
+    leg->Draw();
 
     c.cd(2); // SAD CRTAJ NA sub-pad 2
     hH->GetXaxis()->SetTitle("P_t (GeV/c)");
     //hH->GetYaxis()->SetTitle("Number of particles");
     hH->SetLineColor(kBlack);
-    //hH->SetFillColor(kMagenta);
-    //hH->SetStats(0);
+    hH->SetFillColor(kBlack+2);
     hH->SetTitle("Transverse momentum of Higgs boson");
     hH->Draw();
 
-    //Create legend on current pad (gPad)!
+    //Draw legend on current pad (gPad)!
     //c.cd();
-    TLegend* leg = new TLegend(0.6, 0.7, 0.9, 0.9); // constructor takes coord of lower left and upper right corners
-    leg->SetHeader("Higgs boson decay simulation", "C");
-    leg->AddEntry(h1, "Decay Particle 1", "f");
-    leg->AddEntry(h2, "Decay Particle 2", "l");
+    //leg->SetHeader("Higgs boson decay simulation", "C");
     leg->AddEntry(hH, "Higgs boson", "l");
     leg->Draw();
 
