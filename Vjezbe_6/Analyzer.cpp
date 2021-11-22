f1ff46e3153f8cd030bc83d5bb3a371d74b71392
amsesnic <asesnic@pmfst.hr>
2021-11-22 20:39:16 +0100

    Zad 2: nacrtala sva 4 panela

--- Vjezbe_6/Analyzer.cpp
+++ Vjezbe_6/Analyzer.cpp
@@ -1,5 +1,5 @@
 #define Analyzer_cxx
-#include "Analyzer.h"
+#include <Analyzer.h>
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
@@ -9,51 +9,162 @@ void Analyzer::PlotHistogram()
 {
     TCanvas *c   = new TCanvas("c", "Histogrami", 0, 0, 1000, 1000);
     TCanvas *c_H = new TCanvas("c_H", "Histogram_H", 0, 0, 1000, 1000);
-    TH1F *h1     = new TH1F("h11", "histo11", 100, 0., 100.); //ne znam granice
-    TH1F *h2     = new TH1F("h12", "histo12", 100, 0., 100.); 
-    TH1F *h3     = new TH1F("h13", "histo13", 100, 0., 100.); 
-    TH1F *h4     = new TH1F("h14", "histo14", 100, 0., 100.); 
-    TH1F *h_H    = new TH1F("h_H", "histo_H", 100, 100., 150.); 
+   
+    TH1F *h_Higgs    = new TH1F("h_Higgs", "h_Higgs", 100, 100., 150.);
+    TH1F *h_LepPt[4];
+    TH1F *h_LepEta[4];
+    TH1F *h_LepPhi[4];
+    TH1F *h_LepBDT[4];
+    int i;
+    std::string naslov;
+    for(i=0; i<4; i++){
+        naslov = "h_LepPt"+std::to_string(i);
+        h_LepPt[i]  = new TH1F(naslov.c_str(),  naslov.c_str(),  100, 0., 100.);
+
+        naslov = "h_LepEta"+std::to_string(i);
+        h_LepEta[i] = new TH1F(naslov.c_str(),  naslov.c_str(), 100, 0., 100.);
+
+        naslov = "h_LepPhi"+std::to_string(i);
+        h_LepPhi[i] = new TH1F(naslov.c_str(),  naslov.c_str(), 100, 0., 100.);
+
+        naslov = "h_LepBDT"+std::to_string(i);
+        h_LepBDT[i] = new TH1F(naslov.c_str(),  naslov.c_str(), 100, 0., 100.);
+    }
     
-    //napuni histograme u Loop
-    this->Loop(h1,h2,h3,h4,h_H);
+    /*******   NAPUNI HISTOGRAME U Loop   *******/
+    //Kod kopiran iz Loop() metode
+    if (fChain == 0) return;
+
+    TLorentzVector p_Leptons[4];
+    TLorentzVector p_Z1, p_Z2, p_H4L;
+
+    Long64_t nentries = fChain->GetEntriesFast();
+
+    Long64_t nbytes = 0, nb = 0;
+    for (Long64_t jentry=0; jentry<nentries;jentry++) {
+       Long64_t ientry = LoadTree(jentry);
+       if (ientry < 0) break;
+       nb = fChain->GetEntry(jentry);   nbytes += nb;
+       // if (Cut(ientry) < 0) continue;
+       
+       for(i=0; i<4; i++){
+           h_LepPt[i] ->Fill(LepPt->at(i)); //ovako se pristupa elementu vectora s pokazivacem na taj vector
+           h_LepEta[i]->Fill(LepEta->at(i));
+           h_LepPhi[i]->Fill(LepPhi->at(i));
+           h_LepBDT[i]->Fill(LepBDT->at(i));
+       }
+
+       //4-momentum calculation
+       for(i=0; i<4; i++)
+           p_Leptons[i].SetPtEtaPhiM( LepPt->at(i), LepEta->at(i), LepPhi->at(i), 0.); //masa=0
+      
+       p_Z1 = p_Leptons[0] + p_Leptons[1];
+       p_Z2 = p_Leptons[2] + p_Leptons[3];
+       p_H4L = p_Z1 + p_Z2;
+       h_Higgs ->Fill( p_H4L.M() );
+    }
+    /*****************************************/
 
     c->SetLeftMargin(0.15);
     c->Divide(2,2);
     gStyle->SetOptStat(0);
 
-    /******* PANEL 1 (gore lijevo): Pt *******/
+    /******* PANEL 1 (gore lijevo): LepPt *******/
     c->cd(1);
-    h3->GetXaxis()->SetTitle("Pt (GeV/c)");
-    h3->GetYaxis()->SetTitle("Number of particles");
-    h3->SetLineColor(kBlue+1);
-    h3->SetTitle("Lepton transverse momentum");
-    h3->Draw();
 
-    h4->SetLineColor(kMagenta+2);
-    h4->SetTitle("");
-    h4->Draw("same");
+    h_LepPt[2]->GetXaxis()->SetTitle("Pt (GeV/c)");
+    h_LepPt[2]->GetYaxis()->SetTitle("Number of particles");
+    h_LepPt[2]->SetLineColor(kBlue+1);
+    h_LepPt[2]->SetTitle("Lepton transverse momentum");
+    h_LepPt[2]->Draw();
 
-    h1->SetLineColor(kRed+1);
-    h1->SetTitle("");
-    h1->Draw("same");
+    h_LepPt[3]->SetLineColor(kMagenta+2);
+    h_LepPt[3]->SetTitle("");
+    h_LepPt[3]->Draw("same");
+
+    h_LepPt[0]->SetLineColor(kRed+1);
+    h_LepPt[0]->SetTitle("");
+    h_LepPt[0]->Draw("same");
     
-    h2->SetLineColor(kGreen+2);
-    h2->SetTitle("");
-    h2->Draw("same");
+    h_LepPt[1]->SetLineColor(kGreen+2);
+    h_LepPt[1]->SetTitle("");
+    h_LepPt[1]->Draw("same");
    
+    /******* PANEL 2 (gore desno): LepEta *******/
+    c->cd(2);
+    h_LepEta[2]->GetXaxis()->SetTitle("Eta");
+    h_LepEta[2]->GetYaxis()->SetTitle("Number of particles");
+    h_LepEta[2]->SetLineColor(kBlue+1);
+    h_LepEta[2]->SetTitle("Pseudorapidity");
+    h_LepEta[2]->Draw();
+
+    h_LepEta[3]->SetLineColor(kMagenta+2);
+    h_LepEta[3]->SetTitle("");
+    h_LepEta[3]->Draw("same");
+
+    h_LepEta[0]->SetLineColor(kRed+1);
+    h_LepEta[0]->SetTitle("");
+    h_LepEta[0]->Draw("same");
     
+    h_LepEta[1]->SetLineColor(kGreen+2);
+    h_LepEta[1]->SetTitle("");
+    h_LepEta[1]->Draw("same");
+
+    /******* PANEL 3 (dolje lijevo): LepPhi *******/
+    c->cd(3);
+    h_LepPhi[2]->GetXaxis()->SetTitle("Phi");
+    h_LepPhi[2]->GetYaxis()->SetTitle("Number of particles");
+    h_LepPhi[2]->SetLineColor(kBlue+1);
+    h_LepPhi[2]->SetTitle("Azimuthal angle");
+    h_LepPhi[2]->Draw();
+
+    h_LepPhi[3]->SetLineColor(kMagenta+2);
+    h_LepPhi[3]->SetTitle("");
+    h_LepPhi[3]->Draw("same");
+
+    h_LepPhi[0]->SetLineColor(kRed+1);
+    h_LepPhi[0]->SetTitle("");
+    h_LepPhi[0]->Draw("same");
+    
+    h_LepPhi[1]->SetLineColor(kGreen+2);
+    h_LepPhi[1]->SetTitle("");
+    h_LepPhi[1]->Draw("same");
+
+    /******* PANEL 4 (dolje desno): LepBDT *******/
+    c->cd(4);
+    h_LepBDT[2]->GetXaxis()->SetTitle("BDT");
+    h_LepBDT[2]->GetYaxis()->SetTitle("Number of particles");
+    h_LepBDT[2]->SetLineColor(kBlue+1);
+    h_LepBDT[2]->SetTitle("BDT");
+    h_LepBDT[2]->Draw();
+
+    h_LepBDT[3]->SetLineColor(kMagenta+2);
+    h_LepBDT[3]->SetTitle("");
+    h_LepBDT[3]->Draw("same");
+
+    h_LepBDT[0]->SetLineColor(kRed+1);
+    h_LepBDT[0]->SetTitle("");
+    h_LepBDT[0]->Draw("same");
+    
+    h_LepBDT[1]->SetLineColor(kGreen+2);
+    h_LepBDT[1]->SetTitle("");
+    h_LepBDT[1]->Draw("same");
+
     /*LEGENDA, OSI, OSTALE VARIJABLE, LINIJE, BOJE...*/
     
-    c->Print("H-4l.png");
+    c->Print("4l-distribucije.png");
     
     c_H->cd();
-    h_H->Draw();
+    h_Higgs->Draw();
     c_H->Print("Higgs.png");
-    delete h1;
-    delete h2;
-    delete h3;
-    delete h4;
+
+    for(i=0; i<4; i++){
+        delete h_LepPt[i];
+        delete h_LepEta[i];
+        delete h_LepPhi[i];
+        delete h_LepBDT[i];
+    }
+    delete h_Higgs;
     delete c;
 }
 
@@ -84,8 +195,6 @@ void Analyzer::Loop(TH1F *h1, TH1F *h2, TH1F *h3, TH1F *h4, TH1F *h_H)
 //by  b_branchname->GetEntry(ientry); //read only this branch
    if (fChain == 0) return;
 
-   TLorentzVector p_L1, p_L2, p_L3, p_L4, p_Z1, p_Z2, p_H4L;
-
    Long64_t nentries = fChain->GetEntriesFast();
 
    Long64_t nbytes = 0, nb = 0;
@@ -95,21 +204,6 @@ void Analyzer::Loop(TH1F *h1, TH1F *h2, TH1F *h3, TH1F *h4, TH1F *h_H)
       nb = fChain->GetEntry(jentry);   nbytes += nb;
       // if (Cut(ientry) < 0) continue;
 
-      h1->Fill(LepPt->at(0)); //ovako se pristupa elementu vectora s pokazivacem na taj vector
-      h2->Fill(LepPt->at(1));
-      h3->Fill(LepPt->at(2));
-      h4->Fill(LepPt->at(3));
-
-      //4-momentum calculation
-      p_L1.SetPtEtaPhiM( LepPt->at(0), LepEta->at(0), LepPhi->at(0), 0.); //masa=0
-      p_L2.SetPtEtaPhiM( LepPt->at(1), LepEta->at(1), LepPhi->at(1), 0.);
-      p_L3.SetPtEtaPhiM( LepPt->at(2), LepEta->at(2), LepPhi->at(2), 0.);
-      p_L4.SetPtEtaPhiM( LepPt->at(3), LepEta->at(3), LepPhi->at(3), 0.);
-      
-      p_Z1 = p_L1 + p_L2;
-      p_Z2 = p_L3 + p_L4;
-      p_H4L = p_Z1 + p_Z2;
-      h_H->Fill( p_H4L.M() );
    }
 }
 
