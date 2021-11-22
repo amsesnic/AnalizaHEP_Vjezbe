1391424249d7229f34fbcb7be72cfe1428c7e8eb
amsesnic <asesnic@pmfst.hr>
2021-11-22 21:15:44 +0100

    Zad 2: sredivanje grafa

--- Vjezbe_6/Analyzer.cpp
+++ Vjezbe_6/Analyzer.cpp
@@ -7,8 +7,8 @@
 
 void Analyzer::PlotHistogram()
 {
-    TCanvas *c   = new TCanvas("c", "Histogrami", 0, 0, 1200, 1200);
-    TCanvas *c_H = new TCanvas("c_H", "Histogram_H", 0, 0, 1000, 800);
+    TCanvas *c   = new TCanvas("c", "Histogrami", 0, 0, 800, 800);
+    TCanvas *c_H = new TCanvas("c_H", "Histogram_H", 0, 0, 800, 600);
    
     TH1F *h_Higgs    = new TH1F("h_Higgs", "h_Higgs", 100, 100., 150.);
     TH1F *h_LepPt[4];
@@ -65,7 +65,7 @@ void Analyzer::PlotHistogram()
     }
     /*****************************************/
 
-    c->SetLeftMargin(0.15);
+    c->SetLeftMargin(0.25);
     c->Divide(2,2);
     gStyle->SetOptStat(0);
 
@@ -155,7 +155,7 @@ void Analyzer::PlotHistogram()
     c->Print("4l-distribucije.png");
     
     c_H->cd();
-    h_Higgs->SetTitle("Mass 4 leptons");
+    h_Higgs->SetTitle("4 leptons mass");
     h_Higgs->GetXaxis()->SetTitle("m_4l");
     h_Higgs->GetYaxis()->SetTitle("Number of particles");
     h_Higgs->Draw();
