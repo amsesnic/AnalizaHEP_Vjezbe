458f87701496c787d2575a9e777e90adea6991c9
amsesnic <asesnic@pmfst.hr>
2021-11-23 21:05:07 +0100

    Zad2: pokusaj 7

--- Vjezbe_7/main.cpp
+++ Vjezbe_7/main.cpp
@@ -4,15 +4,17 @@ int main(){
 
     Analyzer *a = new Analyzer();
     TCanvas *c1 = new TCanvas("c1", "higgs i pozadina",0,0, 800, 600);
-    THStack *hstack = new THStack("hstack", "");
-    TLegend *leg = new TLegend(0.6, 0.7, 0.9, 0.9); //koordinate dvaju vrhova
+    THStack *hstack = new THStack("hstack", "Four lepton mass");
+    TLegend *leg = new TLegend(0.7, 0.8, 0.9, 0.9); //koordinate dvaju vrhova
 
     a->PlotHistogram("ggH125");
     a->PlotHistogram("qqZZ"); //sad sam napunila oba histograma za masu 4l    
     
-    hstack->Add(a->h_m4l_higgs);
-    hstack->Add(a->h_m4l_pozadina);
-    hstack->Draw("nostack");
+    hstack->Add(a->h_m4l_pozadina); //prvo dodaj pozadinu
+    hstack->Add(a->h_m4l_higgs);    //onda dodaj higgsa da se zbroji s pozadinom
+    //hstack->GetXaxis()->SetTitle("m_{4l} (GeV)");
+    //hstack->GetYaxis()->SetTitle("Number of events / 2 GeV");
+    hstack->Draw("histo");
     
     leg->AddEntry(a->h_m4l_higgs, "gg->H", "fl");
     leg->AddEntry(a->h_m4l_pozadina, "qq->ZZ", "fl");
