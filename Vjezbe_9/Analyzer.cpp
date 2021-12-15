087e1f012bbdbe837ed8a2c7a414e09fb2e6e886
amsesnic <asesnic@pmfst.hr>
2021-12-15 11:54:37 +0100

    Zad1: tek zapoceto

---
+++ Vjezbe_9/Analyzer.cpp
@@ -0,0 +1,71 @@
+#define Analyzer_cxx
+#include "Analyzer.h"
+
+void Analyzer::PlotHistogram()
+{
+    TCanvas *c   = new TCanvas("c", "Histogram", 0, 0, 1000, 800);
+
+    /*petlja iz Loop()*/
+    if (fChain == 0) return;
+    Long64_t nentries = fChain->GetEntriesFast();
+    Long64_t nbytes = 0, nb = 0;
+    for (Long64_t jentry=0; jentry<nentries;jentry++) {
+        Long64_t ientry = LoadTree(jentry);
+        if (ientry < 0) break;
+        nb = fChain->GetEntry(jentry);   nbytes += nb;
+        // if (Cut(ientry) < 0) continue;
+
+        h_t->Fill(t);
+    }
+
+    gStyle->SetOptFit();
+    gStyle->SetOptStat(0);
+    gPad->SetLeftMargin(0.15);
+
+    h_t->SetTitle("Vrijeme poluraspada");
+    h_t->GetXaxis()->SetTitle("t_{1/2}");
+    h_t->GetYaxis()->SetTitle("Number of events");
+    h_t->SetLineColor(kMagenta+1);
+    h_t->Draw("hist");
+
+    c->SaveAs("poluraspad.png");
+}
+
+
+void Analyzer::Loop()
+{
+//   In a ROOT session, you can do:
+//      root> .L Analyzer.C
+//      root> Analyzer t
+//      root> t.GetEntry(12); // Fill t data members with entry number 12
+//      root> t.Show();       // Show values of entry 12
+//      root> t.Show(16);     // Read and show values of entry 16
+//      root> t.Loop();       // Loop on all entries
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+   if (fChain == 0) return;
+
+   Long64_t nentries = fChain->GetEntriesFast();
+
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+   }
+}
